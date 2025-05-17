8bd86291df024732060b1ffe65c4cbb479223008
glebtrishkin <jamiechong3000@gmail.com>
2025-05-17 16:02:35 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Shop from './pages/Shop';
 import About from './pages/About';
 import Delivery from './pages/Delivery';
+import ProductDetail from './pages/ProductDetail';
+import Cart from './pages/Cart';
+import DomSovets from './pages/DomSovets';
+
+// Component to handle URL parameters for category filtering
+function ShopWithParams() {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const category = searchParams.get('category');
+  
+  return <Shop initialCategory={category} />;
+}
 
 function App() {
   return (
@@ -12,9 +24,12 @@ function App() {
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/shop" element={<Shop />} />
+          <Route path="/shop" element={<ShopWithParams />} />
           <Route path="/about" element={<About />} />
           <Route path="/delivery" element={<Delivery />} />
+          <Route path="/product/:id" element={<ProductDetail />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/dom-sovets" element={<DomSovets />} />
         </Routes>
       </Layout>
     </Router>
